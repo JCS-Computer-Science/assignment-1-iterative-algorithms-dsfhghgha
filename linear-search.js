48e996cbb29426d3431d01d3a7a70f55350f3ec2
Roy Xu <26353004@edu.cbe.ab.ca>
2024-02-13 13:49:08 -0700

    linear and binary searching beast

--- linear-search.js
+++ linear-search.js
@@ -1,10 +1,9 @@
 function linearSearch(array, searchTerm) {
-	/*
-	 ** Check each element from start to finish
-	 ** until you find an element that matches the search term
-	 **
-	 ** Return the index of the found element
-	 */
+	for(let i = 0; i < array.length; i++){
+		if(array[i]===searchTerm){
+			return i;
+		}
+	}
 
 	return null;
 }
